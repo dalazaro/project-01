ade18e60e0d0fd56a7d70e9d3ef8497cff2c03c4
dalazaro <daryl.lazaro@gmail.com>
2017-07-25 15:10:36 -0700

    added endpoints for PUT neighborhood and GET neighborhood by id

--- server.js
+++ server.js
@@ -61,7 +61,22 @@
     })
   });
 
-  // POST api/neighborhood
+  // GET /api/neighborhoods/:id
+  app.get('/api/neighborhood/:id', function(req, res) {
+
+    // get neighborhood id
+    var neighborhoodId = req.params.id;
+    console.log('show neighborhood', neighborhoodId);
+
+    db.Neighborhood.findById(neighborhoodId, function(err, neighborhood) {
+      if(err) {
+        console.log(err);
+      }
+      res.json(neighborhood);
+    })
+  });
+
+  // POST /api/neighborhood
   app.post('/api/neighborhood', function(req, res) {
     // console.log("TESTING THIS OUT");
 
@@ -80,6 +95,33 @@
       console.log('created neighborhood', neighborhood);
       res.json(neighborhood);
     })
+  })
+
+  // PUT /api/neighborhood/:id
+  app.put('/api/neighborhood/:id', function(req, res) {
+
+    // get neighborhood id
+    var neighborhoodId = req.params.id;
+    console.log('show neighborhood', neighborhoodId);
+
+    // find neighborhood
+    db.Neighborhood.findById(neighborhoodId, function(err, neighborhood) {
+      if (err) {
+        return console.log("create error: " + err);
+      }
+      neighborhood.name = req.body.name;
+      neighborhood.wikiUrl = req.body.wikiUrl;
+      neighborhood.restaurants = req.body.restaurants;
+      console.log('updated neighborhood', req.body);
+
+      neighborhood.save(function(err) {
+        if (err) {
+          return console.log("create error: " + err);
+        }
+        res.json({ message: 'Neighborhood updated!' });
+      })
+    })
+
   })
 
 
