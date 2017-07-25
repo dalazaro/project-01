466c36194ff5b4042cedfd4c541ecd6dc0cee1ea
dalazaro <daryl.lazaro@gmail.com>
2017-07-24 21:02:40 -0700

    created git ignore and stopped tracking of node_modules

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/project-01");
+
 // FIXME: make sure current localhost connection string above is working
