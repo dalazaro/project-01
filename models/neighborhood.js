7300bb63312e02b65ab09a686ffe7c23356cee11
stacysn <stacysuen@Stacy-Staces-MacBook-Pro.local>
2017-07-25 13:57:00 -0700

    just saved models (for sake of changing branches)

---
+++ models/neighborhood.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Restaurant = require('./restaurant');
+
+var NeighborhoodSchema = new Schema({
+  name: String,
+  wikiUrl: String,
+  restaurants: [Restaurant.schema]
+});
+
+var Neighborhood = mongoose.model('Neighborhood', NeighborhoodSchema);
+
+module.exports = Neighborhood;
